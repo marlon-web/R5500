2c30ec2d201e0e0afad5fab596b62ace42234c16
Marlon Costa <marlonlCosta.dev@gmail.com>
2021-09-01 12:18:52 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          light: '#4F2B83',
+          DEFAULT: '#3D1E6E',
+          dark: '#3D1E6E',
+        }
+      }
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '950px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
